ebb68b22510a5b6e69b336a8e6c699a9672a313e
harveenchadha <harveensinghchadha@infosys.com>
2017-11-18 23:10:20 +0530

    Changing the winning Score

--- PigGame/app.js
+++ PigGame/app.js
@@ -5,7 +5,7 @@ GAME RULES:
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The first player to reach 11 points on GLOBAL score wins the game
 
 */
 
@@ -29,6 +29,8 @@ function init() {
     document.getElementById('name-0').style.color = "Black";
 
 }
+
+init();
 
 function rollDice() {
     if (stillPlaying == 0) {
@@ -71,7 +73,7 @@ function hold() {
         score[activePlayer] = parseInt(currentScore.textContent) + parseInt(current.textContent);
         currentScore.textContent = score[activePlayer];
         current.textContent = 0;
-        if (parseInt(currentScore.textContent) > 10) {
+        if (parseInt(currentScore.textContent) >= 50) {
             document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
             document.querySelector('#name-' + activePlayer).style.color = "Red";
             stillPlaying = 1;
